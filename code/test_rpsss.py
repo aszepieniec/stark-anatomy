95ee63d0969c44af2d2cd4eb3d072eb1306942a5
Alan <alan.szepieniec@gmail.com>
2021-10-04 18:22:08 +0200

    add timings

--- code/test_rpsss.py
+++ code/test_rpsss.py
@@ -1,19 +1,37 @@
 from rpsss import *
+from time import time
 
 def test_rpsss( ):
     print("Testing R'*K signature scheme ...")
     rpsss = RPSSS()
+
+    tick = time()
     sk, pk = rpsss.keygen()
+    tock = time()
+    print("KeyGen:", (tock - tick), "seconds")
+
     doc = bytes("Hello, world!", "utf-8")
+    tick = time()
     sig = rpsss.sign(sk, doc)
+    tock = time()
+    print("Sign:", (tock - tick), "seconds")
+
+    tick = time()
     valid = rpsss.verify(pk, doc, sig)
+    tock = time()
+    print("Verify:", (tock - tick), "seconds")
+
     if valid:
         print("successfully verified correct signature! \\o/")
     else:
         print("correctly generated signature not valid. <O>")
 
     not_doc = bytes("Byebye.", "utf-8")
+    tick = time()
     valid = rpsss.verify(pk, not_doc, sig)
+    tock = time()
+    print("Verify:", (tock - tick), "seconds")
+
     if valid:
         print("signature authenticates bad document <O>")
     else:
